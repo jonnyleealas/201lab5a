9eb08d100540cb4220ea40f8243b6d110bdbb339
jonathon lee <jonnyleealas@gmail.com>
2020-05-01 20:56:40 -0700

    seans genius

--- starter-code/app.js
+++ starter-code/app.js
@@ -31,14 +31,14 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   
     var product= 'The product of ' + a + ' and '+ b + ' is ';
-    product;
+    
     return [a*b, product + a * b + '.'];
     }
 
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
-
+ 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -55,11 +55,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var addAb= sum(a,b)[0];
+  var addAbc= sum(addAb, c)[0];
+  var ab= multiply(a,b)[0]; 
+  var abc= multiply(ab,c)[0];
+  var string1 = a + ' and ' + b + ' and ' + c + ' sum to ' + addAbc + '.';
+  var string2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + abc + '.';
+  return [addAbc, abc, string1, string2]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
